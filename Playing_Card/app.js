bb9936468d7ff28175366ab118f1201935f5444a
iamemjayo8 <iamemjayo8@users.noreply.github.com>
2021-01-07 19:21:28 +0800

    Update to file

--- Playing_Card/app.js
+++ Playing_Card/app.js
@@ -1,7 +1,7 @@
 class deck {
     constructor() {
         this.cards = [];
-        this.dealcard = null;
+        this.dealcard = "";
         this.dealcards = [];        
     }
     createDeck(){
@@ -41,6 +41,14 @@ const shuffdeck = document.getElementById('deck');
 const dealCardList = document.getElementById('deal-card-list');
 
 const d = new deck();
+var str = "abcd"
+var currentCard = [];
+var prevHistory= []
+var strVal = "";
+var strVal1 = "";
+var strSuit = "";
+var counter = 0;
+var prevNextCounter = 0;
 
 d.createDeck();
 d.shuffleDeck();
@@ -52,8 +60,9 @@ deckValLbl.innerHTML = "Current Deck Card(s) :";
 // console.log(d.dealcards.length);
 dealBtn.addEventListener("click", e => {
     d.dealCard();
+    toWord();
     console.log(d.dealcard);
-    dealValue.innerHTML = d.dealcard;
+    dealValue.innerHTML = d.dealcard + " " + strVal + strSuit;    
     // console.log(d.dealcards.length);
     updateVal();
     if(deckVal.innerText === '52') {
@@ -64,6 +73,8 @@ dealBtn.addEventListener("click", e => {
     } else if(deckVal.innerText === '0') {
         dealBtn.disabled = true;
     }
+    counter += 1;
+    prevNextCounter += 1;
 })
 
 prevBtn.addEventListener("click", e => {
@@ -74,16 +85,26 @@ prevBtn.addEventListener("click", e => {
         let curCardVal = d.dealcards.pop(curLen);
         d.dealcard = curCardVal;
         d.cards.push(curCardVal);
+        prevHistory.push(curCardVal);
         updateVal();
         if(deckVal.innerText === '52') {
             prevBtn.disabled = true;
+            prevNextCounter -= 1;
         }
         nextBtn.disabled = false;
     }
+    
 })
 
 nextBtn.addEventListener("click", e => {
-
+    d.dealCard();
+    updateVal();
+    prevNextCounter += 1;
+    if(counter !== prevNextCounter){   
+        prevBtn.disabled = false;
+    }else{        
+        nextBtn.disabled = true;
+    }
 })
 
 
@@ -91,4 +112,94 @@ function updateVal(){
     shuffdeck.innerHTML = d.cards;
     deckVal.innerHTML = d.cards.length;
     dealCardList.innerHTML = d.dealcards;
+}
+function toWord(){
+    let val, suit;
+    if(d.dealcard.length === 2){        
+        currentCard = d.dealcard.split("");
+        val = currentCard[0];
+        suit = currentCard[1];
+        switch (val) {
+            case 'A':
+                strVal = "Ace of ";
+                break;
+            case 'K':
+                strVal = "King of ";
+                break;
+            case 'Q':
+                strVal = "Queen of ";
+                break;
+            case 'J':
+                strVal = "Jack of ";
+                break;
+            case '2':
+                strVal = "Jack of ";
+                break;
+            case '3':
+                strVal = "Jack of ";
+                break;
+            case '4':
+                strVal = "Jack of ";
+                break;
+            case '5':
+                strVal = "Jack of ";
+                break;
+            case '6':
+                strVal = "Jack of ";
+                break;
+            case '7':
+                strVal = "Jack of ";
+                break;
+            case '8':
+                strVal = "Jack of ";
+                break;
+            case '9':
+                strVal = "Jack of ";
+                break;
+            default:
+                break;
+        }
+        switch (suit) {
+            case '♢':
+                strSuit = "Diamonds";
+                break;
+            case '♡':
+                strSuit = "Hearts";
+                break;
+            case '♠':
+                strSuit = "Spades";
+                break;
+        
+            default:
+                strSuit = "Clubs";
+                break;
+        }
+    }else if(d.dealcard.length === 3){
+        currentCard = d.dealcard.split("");
+        val = d.dealcard[0] + d.dealcard[1];
+        suit = d.dealcard[2];
+        switch (val) {
+            case '10':
+                strVal = "Ten of "
+                break;
+        
+            default:
+                break;
+        }
+        switch (suit) {
+            case '♢':
+                strSuit = "Diamonds";
+                break;
+            case '♡':
+                strSuit = "Hearts";
+                break;
+            case '♠':
+                strSuit = "Spades";
+                break;
+        
+            default:
+                strSuit = "Clubs";
+                break;
+        }
+    }
 }
